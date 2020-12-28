285b106669d5ac1d680f1965cee26e375e8ffeac
lazyzzz <495185365@qq.com>
2020-12-28 17:39:15 +0800

    fix bug - cannot get ip address

--- web/src/main/java/club/lazyzzz/web/security/SysLogAop.java
+++ web/src/main/java/club/lazyzzz/web/security/SysLogAop.java
@@ -5,7 +5,10 @@ import club.lazyzzz.web.model.bo.IpDetail;
 import club.lazyzzz.web.model.entity.SysLog;
 import club.lazyzzz.web.security.annotation.SystemLog;
 import club.lazyzzz.web.service.IIp2RegionService;
+import club.lazyzzz.web.util.IpUtil;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -13,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Aspect
 @Component
@@ -23,25 +28,24 @@ public class SysLogAop {
     @Autowired
     private SysLogMapper logMapper;
 
-    @Around("@annotation(club.lazyzzz.web.security.annotation.SystemLog)")
-    public Object around(ProceedingJoinPoint point) throws Throwable {
-        Object result = point.proceed();
+    @After("@annotation(club.lazyzzz.web.security.annotation.SystemLog)")
+    public void after(JoinPoint point) throws Throwable {
         String description = ((MethodSignature) point.getSignature())
                 .getMethod()
                 .getAnnotation(SystemLog.class)
                 .value();
+
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        String remoteAddr = attributes.getRequest().getRemoteAddr();
-        if ("0:0:0:0:0:0:0:1".equals(remoteAddr)) {
-            remoteAddr = "127.0.0.1";
-        }
-        IpDetail detail = ip2RegionService.getDetail(remoteAddr);
+        HttpServletRequest request = attributes.getRequest();
+        String ipAddress = IpUtil.getIpAddress(request);
+
+        IpDetail detail = ip2RegionService.getDetail(ipAddress);
         SysLog sysLog = new SysLog();
-        sysLog.setIp(remoteAddr);
+        sysLog.setIp(ipAddress);
         sysLog.setCountry(detail.getCountry());
+        sysLog.setProvince(detail.getProvince());
         sysLog.setCity(detail.getCity());
         sysLog.setDescription(description);
         logMapper.insert(sysLog);
-        return result;
     }
 }
---
+++ web/src/main/java/club/lazyzzz/web/util/IpUtil.java
@@ -0,0 +1,49 @@
+package club.lazyzzz.web.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IpUtil {
+
+    private static final String[] HEADERS = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "X-Real-IP"
+    };
+
+    /**
+     * 判断ip是否为空，空返回true
+     *
+     * @param ip
+     * @return 是否为空
+     */
+    public static boolean isEmptyIp(final String ip) {
+        return (ip == null || ip.length() == 0 || ip.trim().equals("") || "unknown".equalsIgnoreCase(ip));
+    }
+
+    /***
+     * 获取客户端ip地址(可以穿透代理)
+     * @param request HttpServletRequest
+     * @return ip地址
+     */
+    public static String getIpAddress(HttpServletRequest request) {
+        String ip = "";
+        for (String header : HEADERS) {
+            ip = request.getHeader(header);
+            if (!isEmptyIp(ip)) {
+                break;
+            }
+        }
+        if (isEmptyIp(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if (!isEmptyIp(ip) && ip.contains(",")) {
+            ip = ip.split(",")[0];
+        }
+        if ("0:0:0:0:0:0:0:1".equals(ip)) {
+            ip = "127.0.0.1";
+        }
+        return ip;
+    }
+
+}
